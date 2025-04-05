77e0a6f3a9f235762c822327a1dac612998986f4
Ejder Dag <ejdrdag@gmail.com>
2025-04-06 01:45:51 +0300

    betterSlep

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ejder Dağ on 4.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
