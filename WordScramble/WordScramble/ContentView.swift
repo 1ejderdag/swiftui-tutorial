6c909c84d435a8a54c3b7ca825cb822903f897ba
Ejder Dag <ejdrdag@gmail.com>
2025-04-22 12:59:10 +0300

    wordScramble

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,173 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Ejder Dağ on 20.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Text(word)
+                            Spacer()
+                            Image(systemName: "\(word.count).circle")
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK") { }
+            } message: {
+                Text(errorMessage)
+            }
+            
+            /*
+            .alert(errorTitle, isPresented: $showingError) { } message: {
+                Text(errorMessage)
+            }*/
+            .toolbar {
+                Button {
+                    self.startGame()
+                } label : {
+                    Image(systemName: "arrow.left.arrow.right")
+                        .fontWeight(.bold)
+                }
+            }
+        }
+    }
+}
+
+extension ContentView {
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        guard conditions(word: answer) else {
+            wordError(title: "too short. Min 3 letter", message: "Long word please")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL, encoding: .utf8) {
+                
+                let allWords = fileContents.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "No word"
+                //print(allWords)
+                
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func conditions(word: String) -> Bool {
+        if word == rootWord || word.count < 3 {
+            return false
+        }
+        return true
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+/*
+ func isPossible(word: String) -> Bool {
+     var tempWord = rootWord
+     var lettersRootWord = Array(rootWord)
+     var lettersNewWord = Array(word)
+   
+     for letter in lettersNewWord {
+         if lettersRootWord.contains(letter) {
+             if let index = lettersRootWord.firstIndex(of: letter) {
+                 lettersRootWord.remove(at: index)
+             }
+         } else {
+             return false
+         }
+     }
+     
+     return true
+ }
+ */
